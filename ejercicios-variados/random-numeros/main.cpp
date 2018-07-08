06d8e38c3ed9a04e02e9bb7408c070e0eea0ae48
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-07-08 18:44:23 -0400

    Añadir funcion para chequear primos

--- ejercicios-variados/random-numeros/main.cpp
+++ ejercicios-variados/random-numeros/main.cpp
@@ -5,13 +5,32 @@
 #include<iostream>
 #include<conio.h>
 
-const int nums = 30;
+const int nums = 25;
 
 using namespace std;
 
 int numAleatorio() {
     int num = rand() % 51 - 25;
     return num;
+}
+
+int chequearPrimo(int n) {
+    bool esPrimo = true;
+
+    if (n < 0) {
+        return 0;
+    }
+
+    for (int i = 2; i <= n / 2; ++i) {
+        if(n % i == 0) {
+            esPrimo = false;
+            break;
+        }
+    }
+
+    if (esPrimo) {
+        return n;
+    }
 }
 
 int main() {
@@ -46,12 +65,24 @@ int main() {
                 break;
             case 2:
                 system("cls");
-                randoms.quitar();
+
+                while(!NRandoms.colaVacia()) {
+                    n = chequearPrimo(NRandoms.quitar());
+
+                    if (n != 0)
+                        NPrimos.insertar(n);
+                }
+
+                cout << "Numeros copiados" << endl;
+
                 getch();
                 break;
             case 3:
                 system("cls");
-                randoms.mostrar();
+                cout << "\nCola de randoms:\n";
+                NRandoms.mostrar();
+                cout << "\nCola de primos:\n";
+                NPrimos.mostrar();
                 getch();
                 break;
             case 4:
